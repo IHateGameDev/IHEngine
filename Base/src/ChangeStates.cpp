f8c135731699832ac4b8824b878fa30816c33d50
andreysemenovih <izaachategamedev@gmail.com>
2025-01-23 10:56:01 +0000

    Test version v0.3: help me

--- Base/src/ChangeStates.cpp
+++
@@ -1,8 +0,0 @@
-#include "../ChangeStates.hpp"
-
-namespace ih
-{
-  ChangeStates::ChangeStates(const uint16& enabled,
-                             const uint16& disabled)
-  : enabled(enabled), disabled(disabled) {}
-} 
